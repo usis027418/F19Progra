c11d10a67a03ec52114bd8975109373d344e25c1
usis027418 <usis027418@ugb.edu.sv>
2020-02-19 10:34:16 -0600

    usis027418

--- app.js
+++ app.js
@@ -1,39 +1,20 @@
+document.addEventListener("DOMContentLoaded", e=>{
+    const form = document.querySelector("#frmConversores");
+    form.addEventListener("submit",event=>{
+        event.preventDefault();
 
-document.addEventListener("DOMContentLoaded", (event) =>{
-    const formAlumnos = document.querySelector("#frmAlumno");
-    formAlumnos.addEventListener("submit", (e)=>{
-        e.preventDefault(); 
-        let codigo = document.querySelector("#txtCodigoAlumno").value,
-            nombre = document.querySelector("#txtNombreAlumno").value,
-            direccion = document.querySelector("#txtDireccionAlumno").value,
-            telefono = document.querySelector("#txtTelefonoAlumno").value;
-        if( 'localStorage' in window ){
-            window.localStorage.setItem("codigo", codigo);
-            window.localStorage.setItem("nombre", nombre);
-            window.localStorage.setItem("direccion", direccion);
-            window.localStorage.setItem("telefono", telefono);
-        } else {
-            alert("almacenamiento en local NO soportado!!! Actualizate!");
-        }
-    });
-    document.querySelector("#btnRecuperarAlumnos").addEventListener("click",(e)=>{
-        if( 'localStorage' in window ){
-            document.querySelector("#txtCodigoAlumno").value = window.localStorage.getItem("codigo");
-            document.querySelector("#txtNombreAlumno").value = window.localStorage.getItem("nombre");
-            document.querySelector("#txtDireccionAlumno").value = window.localStorage.getItem("direccion");
-            document.querySelector("#txtTelefonoAlumno").value = window.localStorage.getItem("telefono");
-        }  else {
-            alert("almacenamiento en local NO soportado!!! Actualizate!");
-        }
+        let de = document.querySelector("#cboDe").value,
+            a = document.querySelector("#cboA").value,
+            cantidad = document.querySelector("#txtCantidadConversor").value;
+        console.log(de, a, cantidad);
+        let monedas = {
+                "dolar":1,
+                "euro":0.93,
+                "quetzal":7.63,
+                "lempira":24.90,
+                "cordoba":34.19
+        };
+        let $res = document.querySelector("#lblRespuesta");
+        $res.innerHTML = `Respuesta: ${ (monedas[a]/monedas[de]*cantidad).toFixed(2) }`;
     });
 });
-
-/*document.addEventListener("DOMContentLoaded",init);*/
-
-/*document.addEventListener("DOMContentLoaded",function(event){
-    alert("Pagina cargo forma 2");
-});*/
-
-/*function init(event){
-    alert("Hola la pagina a cargado");
-}*/
